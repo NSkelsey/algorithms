46c2d08125b4e31d0a303f3be9de93ef1132cb90
NSkelsey <NSkelsey@gmail.com>
2013-10-02 03:34:06 -0400

    more polish

--- select.py
+++ select.py
@@ -11,24 +11,23 @@ G = [19, 9, 301, 2, 4, 5,
      88, 31, 250, 100, 16]
 
 
-def select(A, i, d=1): # A is a list, i is the ith element in that ordered list
+def select(A, i): # A is a list, i is the ith element in that ordered list
                   # NOTE all elements must be unique
     T = list(A); T.sort()
-    print "select on: ", d*'\t',i, T
     if len(A) <= 5: # Base case, just find ith element
         T = list(A)
         T.sort()
         return T[i]
-    p = medianOfMedians(A, d)
+    p = medianOfMedians(A)
     (A_l, A_r, i_p) = partition(A, p)
     if i_p == i:
         return p
     elif i_p < i:
-        return select(A_r, i - i_p - 1, d+1)    
+        return select(A_r, i - i_p - 1)    
     else: # i_p > i
-        return select(A_l, i, d+1)
+        return select(A_l, i)
     
-def medianOfMedians(A,d):
+def medianOfMedians(A):
     B = list()                # B will hold medians of sublists
     itrs, i = len(A)/5, 0
     for j in range(itrs): # divide list into sublists of len 5
@@ -38,10 +37,8 @@ def medianOfMedians(A,d):
         i += 5
     end = len(A) % 5 
     if end != 0:
-        B.append(median(A[-end:]))
-    print "MoM of \t",d*'\t', B
-    p = select(B, len(B)/2, d+1)
-    print "MoM is \t",d*'\t', p
+        B.append(median(A[-end:])) # add the last group to the pot
+    p = select(B, len(B)/2)
     return p
 
 def partition(A, p): # splits the list into left and right half where
@@ -59,29 +56,23 @@ def partition(A, p): # splits the list into left and right half where
 def median(A):  # A cheat to find the median in our base case
     T = list(A) # copies list!
     T.sort()
-    if len(A) % 2 == 0:
-        print "even list"
     i = len(T)/2 #  in list of 4 selects the 3rd element
     return T[int(i)]
     
 def proof(A, i): # naive impl of select to justify correctness
     A = list(A)
     A.sort()
-    print A
     print A[i]
 
 if __name__ == "__main__":
     import sys
     from random import shuffle
-    #A = [3, 4, 9, 1, 2]
     x = int(sys.argv[1])
-    G = range(200)
+    G = range(0,4000,19)
     shuffle(G)
     proof(G, x)
     print '='*20, "My solution", '='*20
-    print(select(G, x, 1))
-    B = [1, 2, 3, 4, 5, 6]
-    #proof(B, 5)
+    print(select(G, x))
 
 
 
