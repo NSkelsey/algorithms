e597eeda5f1158374c7760ff7f13f8d7f5180a7b
NSkelsey <NSkelsey@gmail.com>
2013-10-01 21:09:20 -0400

    computes power a^n in O(log n) mults

---
+++ power.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+// computes a^n in O(log n) multiplications
+// no negatives and nothing that overflows please
+int power(int a, int n) {
+    int r = 1;
+    int temp = a; 
+    for (n; ffs(n) > 0; n = n >> 1) { // ffs finds first set bit, returns 0 if none
+        if (n % 2 == 1) {
+            r = r*temp;
+        }
+        temp = temp*temp;
+    }
+    return r;
+}
+
+int  main (int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <int> <int>\n", argv[0]);
+        exit(0);
+    }
+    int a = atoi(argv[1]);
+    int n = atoi(argv[2]);
+    printf("%d^%d = %d\n", a, n, power(a, n));
+}
